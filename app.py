6b572ed8fddb107d3d5d5102f25594931438f302
alberlome <alberlome@gmail.com>
2025-04-04 03:10:58 +0200

    Finish first release

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, render_template, request
 import requests
-from jinja2 import Environment, FileSystemLoader
 
 app = Flask(__name__)
 
@@ -8,22 +7,34 @@ app = Flask(__name__)
 def helloWorld():
     return render_template('index.html')
 
+def fetch_cve_data(cve):
+    url = f"https://cve.circl.lu/api/vulnerability/{cve}"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            try:
+                data = response.json() 
+                if data is not None:
+                    return data.get("containers", {})
+                else:
+                    return {}
+            except ValueError:
+                return {}
+        else:
+            return {}
+    except requests.exceptions.RequestException as e:
+        print(f"Error en la solicitud: {e}")
+        return {}
+
 @app.route('/cve_results', methods=['POST'])
 def getVulnerabilityForCVE():
-    vulnerability_id = request.form.get('vendor')
-    
-    if not vulnerability_id:
-        return "No se proporcionó el parámetro 'vendor'.", 400
-    
-    # URL para la API
-    url = f"https://cve.circl.lu/api/vulnerability/{vulnerability_id}/"
-    response = requests.get(url)
+    cve = request.form.get('cve')
+    if not cve:
+        return jsonify({"error": "No se proporcionó el parámetro 'cve'."}), 400
+    containers = fetch_cve_data(cve)
+    if not containers:
+        return render_template('cve_results.html', cve=cve, containers=None)
+    return render_template('cve_results.html', cve=cve, containers=containers)
 
-    if response.status_code == 200:
-        vulnerabilities = response.json()  # Esto devuelve la vulnerabilidad detallada
-        return render_template('cve_results.html', vulnerability_id=vulnerability_id, vulnerabilities=vulnerabilities)
-    else:
-        return jsonify({"error": "No se pudo obtener la información."}), 500
-    
 if __name__ == '__main__':
     app.run(debug=True)
